4ca55a45f6f0ac4585ca20aca4fd62c1dccb2826
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-12-17 16:37:36 +0530

    Km_to_Miles.java

---
+++ core-java-practice/gcr-codebase/programming-element/Km_to_Miles.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Km_to_Miles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        System.out.println(k*0.621371);
+    }
+}
