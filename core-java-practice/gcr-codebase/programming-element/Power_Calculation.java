05c17db66935af6745dae86508e49363915fa340
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-12-17 16:31:54 +0530

    Added Power_Calculation.java

---
+++ core-java-practice/gcr-codebase/programming-element/Power_Calculation.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Power_Calculation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        int e = sc.nextInt();
+        System.out.println((int)Math.pow(b,e));
+    }
+}
