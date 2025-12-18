4f67f355af2ed36a2be8b0cc33811be318160f67
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-12-18 11:23:26 +0530

    Sams_Average.java

---
+++ core-java-practice/gcr-codebase/programming-element/Sams_Average.java
@@ -0,0 +1,9 @@
+public class Sams_Average {
+    public static void main(String[] args) {
+        int m = 94;
+        int p = 95;
+        int c = 96;
+        double avg = (m+p+c)/3;
+        System.out.println("Sam's average mark in PCM is "+avg);
+    }
+}
