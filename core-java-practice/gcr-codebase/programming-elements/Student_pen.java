ecb2c7d88e89fff85c09524e4897e07968558cb5
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-12-18 13:01:44 +0530

    02 Problem Number 5 Added

--- core-java-practice/gcr-codebase/programming-elements/Student_pen.java
+++ core-java-practice/gcr-codebase/programming-elements/Student_pen.java
@@ -1,9 +1,9 @@
 public class Student_pen {
     public static void main(String[] args) {
-        int pen = 14;
-        int s = 3;
-        int d = pen/s;
-        int nd = pen%s;
+        int pen = 14; //Number of Pens
+        int s = 3; //Numebr of Students
+        int d = pen/s; //Pens distributed per Student
+        int nd = pen%s; //Remaining Pens
         System.out.println("The Pen Per Student is "+d+" and the remaining pen not distributed is "+nd);
     }
 }
