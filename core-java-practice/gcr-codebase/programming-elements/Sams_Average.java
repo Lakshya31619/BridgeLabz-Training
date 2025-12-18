a6733891c473b2b735ed20e66a650947892c2c38
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-12-18 12:56:58 +0530

    02 Problem Number 2 Added

--- core-java-practice/gcr-codebase/programming-elements/Sams_Average.java
+++ core-java-practice/gcr-codebase/programming-elements/Sams_Average.java
@@ -1,9 +1,9 @@
 public class Sams_Average {
     public static void main(String[] args) {
-        int m = 94;
-        int p = 95;
-        int c = 96;
-        double avg = (m+p+c)/3;
+        int m = 94; //Maths marks
+        int p = 95; //Physics marks
+        int c = 96; //Chemistry marks
+        double avg = (m+p+c)/3; //PCM Average
         System.out.println("Sam's average mark in PCM is "+avg);
     }
 }
