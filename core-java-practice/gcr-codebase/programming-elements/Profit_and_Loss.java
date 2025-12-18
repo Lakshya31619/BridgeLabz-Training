53d5ab149614a4fcf395f762f17cdab075abf0b6
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-12-18 13:00:06 +0530

    02 Problem Number 4 Added

--- core-java-practice/gcr-codebase/programming-elements/Profit_and_Loss.java
+++ core-java-practice/gcr-codebase/programming-elements/Profit_and_Loss.java
@@ -1,9 +1,9 @@
 public class Profit_and_Loss {
     public static void main(String[] args) {
-        int cp = 129;
-        int sp = 191;
-        int p = sp-cp;
-        double pp = (p*100/cp);
+        int cp = 129; //Cost Price
+        int sp = 191; //Selling Price
+        int p = sp-cp; //Profit
+        double pp = (p*100/cp); //Profit Percentage
         System.out.println("The Cost Price is INR "+cp+"and Selling Price is INR "+sp+ "\n" + "The Profit is INR "+p+" and the Profit Percentage is "+pp+"%");
     }
 }
