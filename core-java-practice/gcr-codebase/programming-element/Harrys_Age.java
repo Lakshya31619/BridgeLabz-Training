6ea180ad755291b8ecd066dd2cdd00c8d00950a8
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-12-18 11:21:29 +0530

    Harrys_Age.java

---
+++ core-java-practice/gcr-codebase/programming-element/Harrys_Age.java
@@ -0,0 +1,8 @@
+public class Harrys_Age {
+    public static void main(String[] args) {
+        int b_year = 2000;
+        int c_year = 2024;
+        int age = c_year-b_year;
+        System.out.println("Harry's age in "+ c_year + " is : " + age);
+    }
+}
