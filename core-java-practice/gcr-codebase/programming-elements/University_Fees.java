d85b8ab124f6b72bb07722fd6dbf6ba2048b5463
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-12-18 14:19:30 +0530

    02 Problem Number 6 Added

--- core-java-practice/gcr-codebase/programming-elements/University_Fees.java
+++ core-java-practice/gcr-codebase/programming-elements/University_Fees.java
@@ -2,8 +2,8 @@ public class University_Fees {
     public static void main(String[] args) {
         int fee = 125000;
         int discountPercent = 10;
-        int discount = (discountPercent*fee)/100;
-        int pay = fee-discount;
+        int discount = (discountPercent*fee)/100; //Calculation of discount
+        int pay = fee-discount; //Payable amount
         System.out.println("The discount amount is INR "+discount+" and final discounted fee is INR "+pay);
     }
 }
