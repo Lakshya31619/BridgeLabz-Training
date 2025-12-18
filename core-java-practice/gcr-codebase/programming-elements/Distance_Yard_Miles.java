747d65696d80c901d9a5dd512dfcbd38e0dc55ea
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-12-18 15:53:14 +0530

    02 Problem Number 14 Added

--- core-java-practice/gcr-codebase/programming-elements/Distance_Yard_Miles.java
+++ core-java-practice/gcr-codebase/programming-elements/Distance_Yard_Miles.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class Distance_Yard_Miles {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int distanceFeet = sc.nextInt();
-        double distanceYards = distanceFeet/3.0;
-        double distanceMiles = distanceYards/1760.0;
+        int distanceFeet = sc.nextInt(); //Distance in Feet
+        double distanceYards = distanceFeet/3.0; //Distance in Yards
+        double distanceMiles = distanceYards/1760.0; //Distance in Miles
         System.out.println("Distance in yards is "+distanceYards+" and distance in miles is "+distanceMiles);
     }
 }
