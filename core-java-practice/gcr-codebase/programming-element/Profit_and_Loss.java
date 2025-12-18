36b09ecabe863e9d0e3240c1d8a9dff93e9af9d5
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-12-18 11:25:01 +0530

    Profit_and_Loss.java

---
+++ core-java-practice/gcr-codebase/programming-element/Profit_and_Loss.java
@@ -0,0 +1,9 @@
+public class Profit_and_Loss {
+    public static void main(String[] args) {
+        int cp = 129;
+        int sp = 191;
+        int p = sp-cp;
+        double pp = (p*100/cp);
+        System.out.println("The Cost Price is INR "+cp+"and Selling Price is INR "+sp+ "\n" + "The Profit is INR "+p+" and the Profit Percentage is "+pp+"%");
+    }
+}
