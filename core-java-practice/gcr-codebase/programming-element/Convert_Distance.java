f5a139221e9aa4f0261ace3bc281cbfb05f472e7
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-12-18 11:24:18 +0530

    Convert_Distance.java

---
+++ core-java-practice/gcr-codebase/programming-element/Convert_Distance.java
@@ -0,0 +1,7 @@
+public class Convert_Distance {
+    public static void main(String[] args) {
+        double k = 10.8;
+        double m = k*1.6;
+        System.out.println("The distance "+k+" km in miles is "+m);
+    }
+}
