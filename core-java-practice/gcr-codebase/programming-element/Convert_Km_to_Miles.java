18b5eb9487b30952f0ec7bec74cf1de5f63f8ca3
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-12-18 12:08:51 +0530

    Convert_Km_to_Miles.java

--- core-java-practice/gcr-codebase/programming-element/Convert_Km_to_Miles.java
+++ core-java-practice/gcr-codebase/programming-element/Convert_Km_to_Miles.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class Convert_Km_to_Miles {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        double km = sc.nextDouble();
+        double m = km*0.621371;
+        System.out.println("The total miles is "+m+ " mile for the given "+km+" km");
     }
 }
