38f2684b638340288d0096f8e818266b209dc631
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-12-18 12:55:25 +0530

    02 Problem Number 1 Added

--- core-java-practice/gcr-codebase/programming-elements/Harrys_Age.java
+++ core-java-practice/gcr-codebase/programming-elements/Harrys_Age.java
@@ -1,8 +1,8 @@
 public class Harrys_Age {
     public static void main(String[] args) {
-        int b_year = 2000;
-        int c_year = 2024;
-        int age = c_year-b_year;
+        int b_year = 2000; //birth year
+        int c_year = 2024; //current year
+        int age = c_year-b_year; //current age
         System.out.println("Harry's age in "+ c_year + " is : " + age);
     }
 }
