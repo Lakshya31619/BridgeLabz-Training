b0c1cbda7e391e8f22c0b9f87a1e2db39b99461a
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-12-18 14:22:19 +0530

    02 Problem Number 7 Added

--- core-java-practice/gcr-codebase/programming-elements/Volume_Earth.java
+++ core-java-practice/gcr-codebase/programming-elements/Volume_Earth.java
@@ -1,9 +1,9 @@
 public class Volume_Earth {
     public static void main(String[] args) {
-        int r_km = 6378;
-        double r_m = 6378*0.621371;
-        double volume_km = (4*3.14*Math.pow(r_km,3))/3;
-        double volume_m = (4*3.14*Math.pow(r_m,3))/3;
+        int r_km = 6378; //Radius in kilometers
+        double r_m = 6378*0.621371; //Radius in miles
+        double volume_km = (4*3.14*Math.pow(r_km,3))/3; //Volume in kilometers
+        double volume_m = (4*3.14*Math.pow(r_m,3))/3; //Volume in miles
         System.out.println("The volume of Earth in cubic kilometers is "+volume_km+" and cubic miles is "+volume_m);
     }
 }
