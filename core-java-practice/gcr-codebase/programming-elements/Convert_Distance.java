bf465afb720df5c5138ed311803dc9942243f08e
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-12-18 12:58:42 +0530

    02 Problem Number 3 Added

--- core-java-practice/gcr-codebase/programming-elements/Convert_Distance.java
+++ core-java-practice/gcr-codebase/programming-elements/Convert_Distance.java
@@ -1,7 +1,7 @@
 public class Convert_Distance {
     public static void main(String[] args) {
-        double k = 10.8;
-        double m = k*1.6;
+        double k = 10.8; //Distance in km
+        double m = k*1.6; //Distance in miles
         System.out.println("The distance "+k+" km in miles is "+m);
     }
 }
