45c62ec67486ddddc8b9df56e7d0588fa6a9ba99
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-12-19 16:38:39 +0530

    Add.Added Level 1 Problem Number 12

--- core-java-practice/gcr-codebase/control-flow/CompareSum.java
+++ core-java-practice/gcr-codebase/control-flow/CompareSum.java
@@ -3,17 +3,21 @@ public class CompareSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int sum1 = 0; //For while loop
-        int i = n;
-        while(i!=0){
-            sum1 += i--;
-        }
-        int sum2 = 0; //For formula
-        sum2 = n*(n+1)/2;
-        if(sum1==sum2){
-            System.out.println("Equal");
+        if(n>0){
+            int sum1 = 0; //For while loop
+            int i = n;
+            while(i!=0){
+                sum1 += i--;
+            }
+            int sum2 = 0; //For formula
+            sum2 = n*(n+1)/2;
+            if(sum1==sum2){
+                System.out.println("Equal");
+            }else{
+                System.out.println("Not Equal");
+            }
         }else{
-            System.out.println("Not Equal");
+            System.out.println("Not a natural number");
         }
     }
 }
