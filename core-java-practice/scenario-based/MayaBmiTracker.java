0370323281985869406216e0badc2413117302f9
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-12-20 12:13:15 +0530

    Add.Added Problem 2

---
+++ core-java-practice/scenario-based/MayaBmiTracker.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class MayaBmiTracker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double weight = sc.nextDouble(); //Weight in kg
+        double height = sc.nextInt(); //Height in centimeter
+        double height_m = height/100; //Convert height to meters
+        double bmi = weight/(height_m*height_m); //Formula to generate BMI
+        if(bmi<=18.4){
+            System.out.println("Underweight");
+        }else if(bmi>=18.5 && bmi<=24.9){
+            System.out.println("Normal");
+        }else if(bmi>=25.0 && bmi<=39.9){
+            System.out.println("Overweight");
+        }else if(bmi>=40.0){
+            System.out.println("Obese");
+        }
+    }
+}
