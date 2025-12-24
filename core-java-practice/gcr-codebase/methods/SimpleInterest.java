0f36895b32bf786e31db6321309162ffc443afa7
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-12-24 11:24:14 +0530

    Add.Added Level 1 Problem 1

--- core-java-practice/gcr-codebase/methods/SimpleInterest.java
+++ core-java-practice/gcr-codebase/methods/SimpleInterest.java
@@ -6,9 +6,15 @@ public class SimpleInterest {
         int p = sc.nextInt();
         int r = sc.nextInt();
         int t = sc.nextInt();
-        //Formula for calculating simple interest
-        double si = (p*r*t)/100.0;
+        //Calling simple interest method
+        double si = interest(p, r, t);
         //Output
         System.out.println("The Simple Interest is "+si+" for Principal "+p+", Rate of Interest "+r+" and Time "+t);
+    }
+    //Method simple interest
+    public static double interest(int p, int r, int t){
+        //Formula for calculating simple interest
+        double si = (p*r*t)/100.0;
+        return si;
     }
 }
