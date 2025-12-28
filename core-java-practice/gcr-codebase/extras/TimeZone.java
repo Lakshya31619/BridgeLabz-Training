74d0943dd04b434d3e280370f72ebddf0258e94c
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-12-28 18:41:29 +0530

    Add.Added Extras Problem 13

--- core-java-practice/gcr-codebase/extras/TimeZone.java
+++ core-java-practice/gcr-codebase/extras/TimeZone.java
@@ -3,10 +3,13 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 public class TimeZone {
     public static void main(String[] args) {
+        //DateTimeFormatter to extract Time from ZonedDateTime
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        //ZonedDateTime to fetch GMT. IST and PST
         ZonedDateTime gmt = ZonedDateTime.now(ZoneId.of("GMT"));
         ZonedDateTime ist = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
         ZonedDateTime pst = ZonedDateTime.now(ZoneId.of("America/Vancouver"));
+        //Output
         System.out.println("Current time in GMT : "+gmt.format(formatter));
         System.out.println("Current time in IST : "+ist.format(formatter));
         System.out.println("Current time in PST : "+pst.format(formatter));
