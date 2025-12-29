0e32b4cf2ca4d8a67d93b924968a62947fcd07d5
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-12-29 12:44:20 +0530

    Use Case 2

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,7 +2,9 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         EmployeePresentCheck();
+        EmployeeDailyWage();
     }
+    //Use Case 1
     public static void EmployeePresentCheck(){
         int pick = (int)(Math.random()*2);
         if(pick==0){
@@ -10,5 +12,11 @@ public class EmployeeWage {
         }else{
             System.out.println("Absent");
         }
+    }
+    //Use Case 2
+    public static void EmployeeDailyWage(){
+        int hour = 8;
+        int wagePerHour = 20;
+        System.out.println("Wage per day : "+hour*wagePerHour);
     }
 }
