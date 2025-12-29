db6db9f9a657e5781f44afa155e5c3fb5c43ade2
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-12-29 12:59:18 +0530

    [Lakshya]Add.Added Use Case 4

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,18 +3,20 @@ public class EmployeeWage {
         System.out.println("Welcome to Employee Wage Computation Program");
         int hour = 8;
         int wagePerHour = 20;
-        EmployeePresentCheck();
-        EmployeeDailyWage(hour, wagePerHour);
-        EmployeePartTime(hour, wagePerHour);
-    }
-    //Use Case 1
-    public static void EmployeePresentCheck(){
-        int pick = (int)(Math.random()*2);
+        int pick = EmployeePresentCheck();
         if(pick==0){
             System.out.println("Present");
         }else{
             System.out.println("Absent");
         }
+        EmployeeDailyWage(hour, wagePerHour);
+        EmployeePartTime(hour, wagePerHour);
+        System.out.println(EmployeeSwitchCase(pick, hour, wagePerHour));
+    }
+    //Use Case 1
+    public static int EmployeePresentCheck(){
+        int pick = (int)(Math.random()*2);
+        return pick;
     }
     //Use Case 2
     public static void EmployeeDailyWage(int hour, int wagePerHour){
@@ -23,5 +25,14 @@ public class EmployeeWage {
     //Use Case 3
     public static void EmployeePartTime(int hour, int wagePerHour){
         System.out.println("Part time wage : "+hour*wagePerHour);
+    }
+    //Use Case 4
+    public static int EmployeeSwitchCase(int pick, int hour, int wagePerHour){
+        int partTimeWage = 0;
+        switch(pick){
+            case(0) : partTimeWage += hour*wagePerHour;
+            case(1) : partTimeWage += 0;
+        }
+        return partTimeWage;
     }
 }
