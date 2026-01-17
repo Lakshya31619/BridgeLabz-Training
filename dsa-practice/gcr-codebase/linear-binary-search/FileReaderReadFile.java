fdc219cef84842b14579d79e45f28acdc4a58819
Lakshya Varshney <lakshyavarshney316@gmail.com>
2026-01-17 12:03:50 +0530

    [Lakshya]Add.Added FileReader Problem 1 w/Sample.txt

--- dsa-practice/gcr-codebase/linear-binary-search/FileReaderReadFile.java
+++ dsa-practice/gcr-codebase/linear-binary-search/FileReaderReadFile.java
@@ -3,10 +3,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 public class FileReaderReadFile {
     public static void main(String[] args) {
-        String filepath = "StringBuilderReverse.java";
+        String filepath = "dsa-practice/gcr-codebase/linear-binary-search/sample.txt";
         try{
             FileReader fr = new FileReader(filepath);
             BufferedReader br = new BufferedReader(fr);
+            String line;
+            while((line = br.readLine())!=null){
+                System.out.println(line);
+            }
             br.close();
             fr.close();
         }catch(IOException e){
