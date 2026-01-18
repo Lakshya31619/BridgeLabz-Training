c327388a11e8d3f1353291ea1aadeba350a9bf6f
Lakshya Varshney <lakshyavarshney316@gmail.com>
2026-01-18 23:49:33 +0530

    [Lakshya]Add.Added Problem 0020

---
+++ dsa-practice/leet-code-codebase/ValidParentheses.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        Stack <Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='['){
+                stack.push(s.charAt(i));
+            }else{
+                if(s.charAt(i)==')'){
+                    while(!stack.isEmpty() && stack.peek()=='('){
+                        stack.pop();
+                    }
+                }
+                if(s.charAt(i)=='}'){
+                    while(!stack.isEmpty() && stack.peek()=='{'){
+                        stack.pop();
+                    }
+                }
+                if(s.charAt(i)==']'){
+                    while(!stack.isEmpty() && stack.peek()=='['){
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        String s = "(())";
+        System.out.println(isValid(s));
+    }
+}
