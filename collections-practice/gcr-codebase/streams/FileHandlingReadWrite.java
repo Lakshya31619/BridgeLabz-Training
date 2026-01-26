499cfd47627adbabb5e04663894012bdd169a1c2
Lakshya Varshney <lakshyavarshney316@gmail.com>
2026-01-26 16:18:20 +0530

    [Lakshya]Add.Added Problem 1

---
+++ collections-practice/gcr-codebase/streams/FileHandlingReadWrite.java
@@ -0,0 +1,35 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+public class FileHandlingReadWrite {
+    public static void main(String[] args) {
+        String sourceFile = "./collections-practice/gcr-codebase/streams/source.txt";
+        String destinationFile = "./collections-practice/gcr-codebase/streams/destination.txt";
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        try {
+            fis = new FileInputStream(sourceFile);
+            fos = new FileOutputStream(destinationFile);
+
+            int data;
+            while ((data = fis.read()) != -1) {
+                fos.write(data);
+            }
+            System.out.println("File copied successfully.");
+        } catch (FileNotFoundException e) {
+            System.out.println("Source file does not exist.");
+        } catch (IOException e) {
+            System.out.println("An I/O error occurred: " + e.getMessage());
+        } finally {
+            try {
+                if (fis != null)
+                    fis.close();
+                if (fos != null)
+                    fos.close();
+            } catch (IOException e) {
+                System.out.println("Error while closing the file.");
+            }
+        }
+    }
+}
