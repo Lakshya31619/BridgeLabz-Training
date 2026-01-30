41e7e3da1ffb1530935c92b99ea8e902010db1c4
Lakshya Varshney <lakshyavarshney316@gmail.com>
2026-01-30 14:51:09 +0530

    [Lakshya]Add.Added Advanced Problem 1

--- collections-practice/gcr-codebase/regex/advanced-problems/ValidateIPAddress.java
+++ collections-practice/gcr-codebase/regex/advanced-problems/ValidateIPAddress.java
@@ -3,7 +3,7 @@ public class ValidateIPAddress {
     public static void main(String[] args) {
         String text = "192.168.1.1";
         String regex = "^((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)\\.){3}"+"(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)$";
-        Pattern pattern = Pattern.compile(regex);
+        Pattern pattern = Pattern.compile(regex); 
         System.out.println(pattern.matcher(text).matches());
     }
 }
