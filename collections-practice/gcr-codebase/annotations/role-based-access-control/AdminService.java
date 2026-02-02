2c50c6f3fd37c61d69c641f29d5312ca08108ba5
Lakshya Varshney <lakshyavarshney316@gmail.com>
2026-02-02 18:21:04 +0530

    [Lakshya]Add.Added Problem 10

---
+++ collections-practice/gcr-codebase/annotations/role-based-access-control/AdminService.java
@@ -0,0 +1,9 @@
+@RoleAllowed("ADMIN")
+public class AdminService {
+    public void deleteUser() {
+        System.out.println("User deleted successfully!");
+    }
+    public void generateReport() {
+        System.out.println("Admin report generated!");
+    }
+}
