69a0f69b700b0b3ce8ff7d4b4d21fd605f7fbf4d
Lakshya Varshney <lakshyavarshney316@gmail.com>
2026-02-16 14:50:16 +0530

    [Lakshya]Refactored Folder Structure

--- oops-practice/gcr-codebase/address-book-system/dao/AddressBookDAO.java
+++
@@ -1,6 +0,0 @@
-import java.util.List;
-public interface AddressBookDAO {
-    void addContact(ContactInfo person);
-    void removeContact(String fullName);
-    List<ContactInfo> getAllContacts();
-}
